dbb3e8ce24fbb510f2ab75d9bbaf47bc014ec8b7
Combatman96 <84495690+Combatman96@users.noreply.github.com>
2021-12-18 10:56:33 +0700

    LevelSelectScene added
    Thêm scene để chọn level đồng thời tạo model để lưu các màn đã được unlock

---
+++ Assets/Script/Level/LevelSelectController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelSelectController : MonoBehaviour
+{
+    [Header("Level Buttons")] 
+    public Button[] levelBtn = new Button[6];
+    
+    [Header("Model")] 
+    public LevelUnlockedData levelUnlockedData;
+
+    private void Start()
+    {
+        //Make buttons which load unlocked levels interactable
+        for (int i = 2; i < 7; i++)
+        {
+            string levelName = "Level" + i;
+            if (levelUnlockedData.GetLevelStatus(levelName) == 1)
+            {
+                levelBtn[i-1].interactable = true;
+            }
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            //Back to Title Screen
+            SceneManager.LoadScene("TitleScreen", LoadSceneMode.Single);
+        }
+    }
+
+    public void OnLevelBtnClicked(int lv)
+    {
+        //Load the Level lv Scene
+        switch (lv)
+        {
+            case 1:
+                //Load Level1
+                break;
+            case 2:
+                //Load Level2
+                break;
+            case 3:
+                //Load Level3
+                break;
+            case 4:
+                //Load Level4
+                break;
+            case 5:
+                //Load Level5
+                break;
+            case 6:
+                //Load Level6
+                break;
+        }
+    }
+}
---
+++ Assets/Script/Level/LevelUnlockedData.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+public class LevelUnlockedData : MonoBehaviour
+{
+    static int lv1 = 1;
+    static int lv2 = 0;
+    static int lv3 = 0;
+    static int lv4 = 0;
+    static int lv5 = 0;
+    static int lv6 = 0;
+
+    private void Start()
+    {
+        PlayerPrefs.SetInt("Level1", lv1);
+        PlayerPrefs.SetInt("Level2", lv2);
+        PlayerPrefs.SetInt("Level3", lv3);
+        PlayerPrefs.SetInt("Level4", lv4);
+        PlayerPrefs.SetInt("Level5", lv5);
+        PlayerPrefs.SetInt("Level6", lv6);
+    }
+
+    public void UnlockLevel(int lvNum)
+    {
+        switch (lvNum)
+        {
+            case 2:
+                lv2 = 1;
+                PlayerPrefs.SetInt("Level2", lv2);
+                break;
+            case 3:
+                lv3 = 1;
+                PlayerPrefs.SetInt("Level3", lv3);
+                break;
+            case 4:
+                lv4 = 1;
+                PlayerPrefs.SetInt("Level4", lv4);
+                break;
+            case 5:
+                lv5 = 1;
+                PlayerPrefs.SetInt("Level5", lv5);
+                break;
+            case 6:
+                lv6 = 1;
+                PlayerPrefs.SetInt("Level6", lv6);
+                break;
+        }
+    }
+
+    public int GetLevelStatus(string levelName)
+    {
+        return PlayerPrefs.GetInt(levelName);
+    }
+    
+}
